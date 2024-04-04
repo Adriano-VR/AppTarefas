2ee07d7925c40d79946f9b8599fcafde22e4a8c2
Adriano <rodrigues.adriano2607@gmail.com>
2024-04-04 20:21:11 -0300

    add modal foto

--- src/screens/TaskDetails.tsx
+++ src/screens/TaskDetails.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
-import React, { useContext } from "react";
-import { Button, FlatList, Image, Text, View,StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { Button, FlatList, Image, Text, View, StyleSheet, Alert, Modal, Pressable } from "react-native";
 import { Task } from "../types/Task";
 import { TaskContext } from "../contexts/TaskContext";
 import { colors } from "../Colors/colors";
@@ -14,10 +14,14 @@ import { FontAwesome } from "@expo/vector-icons";
 const TaskDetails = () => {
   const route = useRoute<any>();
   const { takePhoto, pickImage, images } = useContext(TaskContext);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null); // Estado para armazenar a imagem clicada
+
+
   const { id, title, completed, category, date, image } = route.params as Task;
 
   // const categoryColor = categories.find((cat) => cat.value === category)
-  
+
 
   const handleTakePhoto = () => {
     takePhoto(id);
@@ -27,18 +31,24 @@ const TaskDetails = () => {
     pickImage(id);
   };
 
+  const handleImageClick = (clickedImage: string) => {
+    setSelectedImage(clickedImage);
+    setModalVisible(true);
+  };
+
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         backgroundColor: colors.cor2,
-       
+
       }}
     >
-      <View      
+      <View
         style={{
-        borderWidth: 1,
+          borderWidth: 1,
           borderRadius: 10,
           padding: 15,
           borderColor: completed === 1 ? "green" : "red",
@@ -49,11 +59,11 @@ const TaskDetails = () => {
           width: `90%`,
           alignItems: "center",
         }}
-      
->
- 
-        <View style={{flexDirection:'row',justifyContent:'center'}}>
-        
+
+      >
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+
           <Text
             style={{
               color: "white",
@@ -63,13 +73,13 @@ const TaskDetails = () => {
               paddingBottom: 25,
             }}
           >
-           {title}
+            {title}
           </Text>
           <Text
             style={{
               color: "white",
               fontSize: 25,
-            
+
             }}
           >
             {/* {completed === 1 ? (
@@ -93,22 +103,25 @@ const TaskDetails = () => {
 
         {/* <Text>{image}</Text> */}
 
-        <FlatList 
+
+
+        <FlatList
           data={images}
           renderItem={({ item }) => (
-         
-            <Image
-              style={{ height: 80, width: 80,borderWidth: 1,borderColor: "gray",}}
-              source={{ uri: "data:image/jpeg;base64," + item }}
-            />
-        
-           
-           
+
+            <TouchableOpacity onPress={() => handleImageClick(item)}>
+              <Image
+                style={{ height: 80, width: 80, borderWidth: 1, borderColor: "gray", }}
+                source={{ uri: "data:image/jpeg;base64," + item }}
+              />
+            </TouchableOpacity>
+
+
+
           )}
           keyExtractor={(item, index) => index.toString()}
           numColumns={4}
         />
-
         <View
           style={{
             flexDirection: "row",
@@ -116,36 +129,69 @@ const TaskDetails = () => {
             justifyContent: "space-between",
           }}
         >
-          <View style={{flex:1, flexDirection: "row", gap: 20,justifyContent: "space-between"}}> 
-          <View>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 15,
-              }}
-            >
-              {date}
-            </Text>
-          </View>
+          <View style={{ flex: 1, flexDirection: "row", gap: 20, justifyContent: "space-between" }}>
+            <View>
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 15,
+                }}
+              >
+                {date}
+              </Text>
+            </View>
 
-          <View style={{ flexDirection: "row", gap: 20 }}>
-            <TouchableOpacity onPress={handleTakePhoto}>
-              <MaterialIcons name="add-a-photo" size={24} color="white" />
-            </TouchableOpacity>
+            <View style={{ flexDirection: "row", gap: 20 }}>
+              <TouchableOpacity onPress={handleTakePhoto}>
+                <MaterialIcons name="add-a-photo" size={24} color="white" />
+              </TouchableOpacity>
 
-            <TouchableOpacity onPress={handlePickImage}>
-              <FontAwesome name="file-photo-o" size={24} color="white" />
-            </TouchableOpacity>
-          </View>
+              <TouchableOpacity onPress={handlePickImage}>
+                <FontAwesome name="file-photo-o" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+
+          <Image
+            style={{ height: '90%', width: '90%', resizeMode: 'contain' }}
+            source={{ uri: "data:image/jpeg;base64," + selectedImage }}
+          />
+
+          <Pressable onPress={() => setModalVisible(false)}>
+            <Text style={{ color: 'white', fontSize: 24 }}>Fechar</Text>
+          </Pressable>
+        </View>
+      </Modal>
     </View>
+
   );
 };
 
- 
 
-  
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
+  },
+});
+
 
 export default TaskDetails;
