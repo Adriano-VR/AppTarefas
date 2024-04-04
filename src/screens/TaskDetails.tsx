166a2b6ec45ccc74542cbb1587d7317549069937
Adriano2607 <rodrigues.adriano2607@gmail.com>
2024-04-04 19:54:42 -0300

    fix errors

--- src/screens/TaskDetails.tsx
+++ src/screens/TaskDetails.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
 import React, { useContext } from "react";
-import { Button, FlatList, Image, Text, View } from "react-native";
+import { Button, FlatList, Image, Text, View,StyleSheet } from "react-native";
 import { Task } from "../types/Task";
 import { TaskContext } from "../contexts/TaskContext";
 import { colors } from "../Colors/colors";
@@ -8,6 +8,8 @@ import { categories } from "../utils/data/data";
 import { MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
+
+
 
 const TaskDetails = () => {
   const route = useRoute<any>();
@@ -31,25 +33,26 @@ const TaskDetails = () => {
         flex: 1,
         justifyContent: "center",
         backgroundColor: colors.cor2,
+       
       }}
     >
-      <View
+      <View      
         style={{
-          borderBottomWidth: 3,
-          borderRightWidth: 1,
-          borderTopWidth: 3,
-          borderLeftWidth: 1,
+        borderWidth: 1,
           borderRadius: 10,
           padding: 15,
-          borderColor: '#252525',
+          borderColor: completed === 1 ? "green" : "red",
           backgroundColor: colors.cor3,
           alignSelf: "center",
           justifyContent: "space-between",
-          height: 650,
-          width: 350,
+          height: '90%',
+          width: `90%`,
+          alignItems: "center",
         }}
-      >
-        <View style={{flexDirection:'row',justifyContent:'center',gap:30}}>
+      
+>
+ 
+        <View style={{flexDirection:'row',justifyContent:'center'}}>
         
           <Text
             style={{
@@ -69,11 +72,11 @@ const TaskDetails = () => {
             
             }}
           >
-            {completed === 1 ? (
+            {/* {completed === 1 ? (
               <FontAwesome name="check" size={40} color="green" />
             ) : (
               <FontAwesome name="close" size={40} color="red" />
-            )}
+            )} */}
           </Text>
         </View>
 
@@ -90,15 +93,20 @@ const TaskDetails = () => {
 
         {/* <Text>{image}</Text> */}
 
-        <FlatList
+        <FlatList 
           data={images}
           renderItem={({ item }) => (
+         
             <Image
-              style={{ height: 70, width: 70 }}
+              style={{ height: 80, width: 80,borderWidth: 1,borderColor: "gray",}}
               source={{ uri: "data:image/jpeg;base64," + item }}
             />
+        
+           
+           
           )}
           keyExtractor={(item, index) => index.toString()}
+          numColumns={4}
         />
 
         <View
@@ -108,6 +116,7 @@ const TaskDetails = () => {
             justifyContent: "space-between",
           }}
         >
+          <View style={{flex:1, flexDirection: "row", gap: 20,justifyContent: "space-between"}}> 
           <View>
             <Text
               style={{
@@ -128,10 +137,15 @@ const TaskDetails = () => {
               <FontAwesome name="file-photo-o" size={24} color="white" />
             </TouchableOpacity>
           </View>
+          </View>
         </View>
       </View>
     </View>
   );
 };
+
+ 
+
+  
 
 export default TaskDetails;
